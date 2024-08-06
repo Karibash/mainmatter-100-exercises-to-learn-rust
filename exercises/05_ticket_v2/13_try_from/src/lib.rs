49637ee4adb7fb1035090a4676333270289be119
Karibash <y.karibayashi@gmail.com>
2024-08-06 15:17:44 +0900

    feature: Add an answer to the 05_ticket_v2_13_try_from

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -1,11 +1,37 @@
-// TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for `Status`.
-//  The parsing should be case-insensitive.
-
 #[derive(Debug, PartialEq, Clone)]
 enum Status {
     ToDo,
     InProgress,
     Done,
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("{value} is not a valid status")]
+struct StatusCreateError {
+    value: String,
+}
+
+impl TryFrom<String> for Status {
+    type Error = StatusCreateError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = StatusCreateError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err(StatusCreateError {
+                value: value.to_string(),
+            }),
+        }
+    }
 }
 
 #[cfg(test)]
